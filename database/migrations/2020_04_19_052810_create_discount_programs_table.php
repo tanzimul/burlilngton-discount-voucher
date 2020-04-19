8b1ff8b7dddf77c4738092fb8f2ee10390019ab9
Tanzim <tanzimul.tanim@gmail.com>
2020-04-19 12:41:01 +0600

    Database created

---
+++ database/migrations/2020_04_19_052810_create_discount_programs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscountProgramsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discount_programs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('membership_id');
+            $table->string('membership_type','10');
+            $table->integer('discount_id');
+            $table->string('device','20')->nullable();
+            $table->smallInteger('print_count');
+            $table->boolean('is_used');
+            $table->boolean('is_admin');
+            $table->dateTime('used_at', 0);
+            $table->timestamps();
+
+            $table->foreign('membership_id')->references('id')->on('members');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discount_programs');
+    }
+}
--- database/migrations/2020_04_19_055902_create_news_letters_table.php
+++ database/migrations/2020_04_19_055902_create_news_letters_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateNewsLettersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('news_letters', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->unsignedBigInteger('membership_id');
             $table->string('email')->unique();
-            $table->smallInteger('role');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->boolean('is_email_sent');
             $table->timestamps();
+
+            $table->foreign('membership_id')->references('id')->on('members');
         });
     }
 
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('news_letters');
     }
 }
